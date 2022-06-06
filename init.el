a9ec6f96e33034bdeaf7dc5918e0ca1823e6682f
Sasha Yee <yee.sasha@abc.net.au>
2022-06-06 10:48:30 +1000

    Added benchmarking functionality

--- init.el
+++ init.el
@@ -43,6 +43,13 @@
     (require 'use-package)))
 (setq use-package-always-ensure t)
 
+;; Benchmark startup times
+;; (use-package benchmark-init
+;;   :ensure t
+;;   :config
+;;   ;; To disable collection of benchmark data after init is done.
+;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 (use-package evil
   :init
 
