6fef514e791293903840c1c4bc063f7758094f81
Sasha Yee <yee.sasha@abc.net.au>
2024-01-23 21:56:45 +1100

    add dape package

--- modules/my-coding.el
+++ modules/my-coding.el
@@ -318,6 +318,36 @@
   (add-hook 'jtsx-jsx-mode-hook 'jtsx-bind-keys-to-jtsx-jsx-mode-map)
   (add-hook 'jtsx-tsx-mode-hook 'jtsx-bind-keys-to-jtsx-tsx-mode-map))
 
+
+(use-package dape
+  :defer t
+  ;; To use window configuration like gud (gdb-mi)
+  ;; :init
+  ;; (setq dape-buffer-window-arrangment 'gud)
+  :config
+  ;; Info buffers to the right
+  (setq dape-buffer-window-arrangment 'right)
+
+  ;; To remove info buffers
+  ;; (remove-hook 'dape-update-ui-hooks 'dape-info-update)
+
+  ;; To remove repl buffer on startup
+  ;; (remove-hook 'dape-on-start-hooks 'dape-repl)
+
+  ;; By default dape uses gdb keybinding prefix
+  (setq dape-key-prefix "\C-x\C-a")
+
+  ;; Kill compile buffer on build success
+  ;; (add-hook 'dape-compile-compile-hooks 'kill-buffer)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  ;; (add-hook 'dape-on-start-hooks
+  ;;           (defun dape--save-on-start ()
+  ;;             (save-some-buffers t t)))
+
+  ;; Projectile users
+  (setq dape-cwd-fn 'projectile-project-root))
+
 ;;; Package:
 (provide 'my-coding)
 ;;; my-coding.el ends here
