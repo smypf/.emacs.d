2e65d294ba51c4b35fcdf611cba6478afe3a8eee
Sasha Yee <yee.sasha@abc.net.au>
2024-01-23 22:01:19 +1100

    formatting

--- modules/my-org.el
+++ modules/my-org.el
@@ -15,37 +15,41 @@
   :defer t
   :config
   (setq org-directory (file-truename "~/org")
-    org-agenda-files (directory-files-recursively org-directory "\\.org$")
-    org-default-notes-file (concat org-directory "/notes.org")
-    org-return-follows-link t
-    ;; Add toggling states and different states
-    ;; e.g <SPC> m t t => TODO
-    ;; e.g <SPC> m t d => Done
-    ;; These key binds will conflict with meow, where <SPC> m becomes M-
-    ;; This is achieved with org-todo and having a (letter) at the end of the keyword.
-    ;; (setq org-todo-keywords
-    ;;      '((sequence "TODO" "WAIT" "|" "DONE" "CANCELLED")))
-    org-todo-keywords '((sequence
-                         "TODO(t)"  ; A task that needs doing & is ready to do
-                         "WAIT(w)"  ; Something external is holding up this task
-                         "HOLD(h)"  ; This task is paused/on hold because of me
-                         "IDEA(i)"  ; An unconfirmed and unapproved task or notion
-                         "|"
-                         "DONE(d)"  ; Task successfully completed
-                         "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
-                        (sequence
-                         "[ ](T)"   ; A task that needs doing
-                         "[-](S)"   ; Task is in progress
-                         "[?](W)"   ; Task is being held up or paused
-                         "|"
-                         "[X](D)")) ; Task was completed
-    ;; Ensure that a heading and it's contents are automatically aligned
-    org-adapt-indentation t
-    org-log-done 'time
-    org-capture-templates '(("t" "Task" entry (file+headline org-default-notes-file "Tasks") "* TODO %?\n  %T")
-                            ("a" "Emacs Annoyance" entry (file+headline "~/org/emacs.org" "Annoyances") "* TODO %?\n  %T")
-                            ("i" "Emacs Improvement" entry (file+headline "~/org/emacs.org" "Sharp tools") "* TODO %?\n  %T")
-                            ("r" "Reading" entry (file "~/org/reading.org") "* %?\n")))
+        org-agenda-files (directory-files-recursively org-directory "\\.org$")
+        org-default-notes-file (concat org-directory "/notes.org")
+        org-return-follows-link t
+        ;; This results in prettier content without unnecessary spaces in files
+        ;; https://www.reddit.com/r/emacs/comments/97naje/comment/e4a6qqt
+        org-indent-indentation-per-level 0
+        org-startup-indented t
+        ;; Add toggling states and different states
+        ;; e.g <SPC> m t t => TODO
+        ;; e.g <SPC> m t d => Done
+        ;; These key binds will conflict with meow, where <SPC> m becomes M-
+        ;; This is achieved with org-todo and having a (letter) at the end of the keyword.
+        ;; (setq org-todo-keywords
+        ;;      '((sequence "TODO" "WAIT" "|" "DONE" "CANCELLED")))
+        org-todo-keywords '((sequence
+                             "TODO(t)"  ; A task that needs doing & is ready to do
+                             "WAIT(w)"  ; Something external is holding up this task
+                             "HOLD(h)"  ; This task is paused/on hold because of me
+                             "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+                             "|"
+                             "DONE(d)"  ; Task successfully completed
+                             "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+                            (sequence
+                             "[ ](T)"   ; A task that needs doing
+                             "[-](S)"   ; Task is in progress
+                             "[?](W)"   ; Task is being held up or paused
+                             "|"
+                             "[X](D)")) ; Task was completed
+        ;; Ensure that a heading and it's contents are automatically aligned
+        org-adapt-indentation t
+        org-log-done 'time
+        org-capture-templates '(("t" "Task" entry (file+headline org-default-notes-file "Tasks") "* TODO %?\n  %T")
+                                ("a" "Emacs Annoyance" entry (file+headline "~/org/emacs.org" "Annoyances") "* TODO %?\n  %T")
+                                ("i" "Emacs Improvement" entry (file+headline "~/org/emacs.org" "Sharp tools") "* TODO %?\n  %T")
+                                ("r" "Reading" entry (file "~/org/reading.org") "* %?\n")))
   (add-hook 'org-mode-hook 'turn-on-auto-fill))
 
 ;; Org Roam doesn't work for me.
