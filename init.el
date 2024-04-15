45f038998d8444362273def3926cc3944c88dfe6
Sasha Yee <yee.sasha@abc.net.au>
2024-04-15 20:46:48 +1000

    add settigns to speed up emacs initialisation

--- init.el
+++ init.el
@@ -4,6 +4,30 @@
 ;;; Most of the stuff is in module files.  These are located in the `/module` folder
 ;;; Code:
 
+
+;; settings to speedup initialisation
+(setq gc-cons-threshold (* 32 1024 1024))
+(setq gcmh-high-cons-threshold (* 32 1024 1024))
+(setq gcmh-idle-delay-factor 20)
+(setq jit-lock-defer-time 0.05)
+(setq read-process-output-max (* 1024 1024))
+(setq package-native-compile t)
+
+(defun my-minibuffer-setup-hook ()
+  (setq gc-cons-threshold most-positive-fixnum))
+
+(defun my-minibuffer-exit-hook ()
+  (setq gc-cons-threshold (* 32 1024 1024)))
+
+(add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
+(add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
+
+;; if you don't use RTL ever, this could improve perf
+(setq-default bidi-display-reordering 'left-to-right
+              bidi-paragraph-direction 'left-to-right
+              bidi-inhibit-bpa t)
+
+
 ;; Set up melpa which is a package host
 (require 'package)
 (add-to-list 'package-archives
