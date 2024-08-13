8f1674ffc8d61f884efa5dc723ae6c8d8d69ef6a
Sasha Yee <syee@atlassian.com>
2024-08-13 16:03:51 +1000

    add deadgrep

--- init.el
+++ init.el
@@ -81,6 +81,7 @@
 (require 'my-golang)
 (require 'my-restclient)
 (require 'my-atlassian)
+(require 'my-deadgrep)
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
