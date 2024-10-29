33ec8a0f01042f75182bafe253e0f63775585faa
Sasha Yee <syee@atlassian.com>
2024-10-29 12:14:28 +1100

    Change appearance to be nicer. The missing titlebar wasn't working for me.

--- early-init.el
+++ early-init.el
@@ -1,8 +1,15 @@
 ;; Prevent pauses due to garbage collection by increasing the threshold
 (setq gc-cons-threshold (* 50 1000 1000))
 
-(unless window-system
-  (add-to-list 'default-frame-alist '(undecorated . t)))
+
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . light)) ;; assuming you are using a dark theme
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
+(tool-bar-mode -1)
+
+;; (unless window-system
+;;  (add-to-list 'default-frame-alist '(undecorated . t)))
 
 ;; Uncomment these lines to determine startup time
 ;;   (add-hook 'emacs-startup-hook
